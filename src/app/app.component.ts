a13b1a9da7af973370e143a5d4cecb12897edbb2
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-03 22:44:36 +0100

    Forms [193] Add default value with help of [ngModel]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f', {static: true}) signupForm: NgForm;
 
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
