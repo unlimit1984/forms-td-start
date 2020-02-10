1bb2128c977175128fa67eb97de7ba0a4897bf36
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-10 20:29:42 +0100

    Forms [199] Resetting the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,5 +57,7 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
     this.submitted = true;
+
+    this.signupForm.reset();
   }
 }
