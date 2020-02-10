6e3913a1f1b9beb982ac251b69764d5743d06cf5
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-10 20:23:20 +0100

    Forms [198] Reading ngForm data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,15 @@ export class AppComponent {
   answer = '';
   genders = ['male', 'female'];
 
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
     // Nice but not a good approach
@@ -41,6 +50,12 @@ export class AppComponent {
   // }
   //  2nd option
   onSubmit() {
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
   }
 }
